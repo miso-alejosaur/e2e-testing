fed00434e85cff090ca0ee14ab323c29bf3aa556
miso-alejosaur <a.santamaria11@uniandes.edu.co>
2022-05-06 15:56:42 -0500

    avances

--- kraken/features/create_posts.feature
+++ kraken/features/create_posts.feature
@@ -1,8 +1,36 @@
 Feature:
 
+#@user1 @web
+#Scenario: Como autor creo un post y lo verifico desde la vista usuario
+#    Given I navigate to page "<HOST>" "ghost/#/signin"
+#    And I wait for 1 seconds
+#    When I login with credentials "<USERNAME>" "<PASSWORD>"
+#    And I go to new post form
+#    And I create a post with title "$name_1" and content "$string_1"
+#    And I post the post
+#    And I navigate to page "<HOST>"
+#    And I select the post with title "$$name_1"
+#    Then I check the post name "$$name_1"
+#    And I check the post content "$$string_1"
+#    And I wait for 10 seconds
+
+
 @user1 @web
-Scenario: Como autor creo un post y lo verifico desde la vista usuario
+Scenario: Como autor creo un tag, creo un post con tag y lo verifico filtrando desde la vista usuario
     Given I navigate to page "<HOST>" "ghost/#/signin"
     And I wait for 1 seconds
     When I login with credentials "<USERNAME>" "<PASSWORD>"
+    And I go to tag list
+
+    And I go to new tag form
+    And I create a tag with name "$name_1"
+    And I wait for 100 seconds
+
+    And I go to new post form
+    And I create a post with title "$name_1" and content "$string_1"
+    And I post the post
+    And I navigate to page "<HOST>"
+    And I select the post with title "$$name_1"
+    Then I check the post name "$$name_1"
+    And I check the post content "$$string_1"
     And I wait for 10 seconds
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,4 +1,5 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+const expect = require('chai').expect;
 
 Given, When('I navigate to page {kraken-string} {kraken-string}', async function (host, url) {
     return await this.driver.url(host+url);
@@ -12,4 +13,55 @@ When('I login with credentials {kraken-string} {kraken-string}', async function 
     new Promise(r => setTimeout(r, 300))
     let elementLoginButton = await this.driver.$("#ember11");
     return elementLoginButton.click();
+});
+
+When('I go to new post form', async function () {
+    let elementNewPost = await this.driver.$(".gh-nav-new-post");
+    return elementNewPost.click();
+});
+
+When('I create a post with title {kraken-string} and content {kraken-string}', async function (title, content) {
+    let elementTitle = await this.driver.$(".gh-editor-title");
+    await elementTitle.setValue(title);
+    let elementContent = await this.driver.$(".koenig-editor__editor");
+    await elementContent.setValue(content);
+});
+
+When('I post the post', async function () {
+    let publishDropdown = await this.driver.$(".ember-basic-dropdown-trigger");
+    await publishDropdown.click();
+    let publishButton = await this.driver.$(".gh-publishmenu-button");
+    await publishButton.click();
+    let modalButton = await this.driver.$(".modal-content > .modal-footer > .gh-btn-black");
+    await modalButton.click();
+});
+
+When('I select the post with title {kraken-string}', async function (title) {
+    let postItem = await this.driver.$(".//*//h2[text() = '" + title + "']");
+    await postItem.click();
+});
+
+When('I go to tag list', async function () {
+    let elementTagsButton = await this.driver.$("a[href='#/tags/']");
+    return elementTagsButton.click();
+});
+
+When('I go to new tag form', async function () {
+    let elementNewTag = await this.driver.$("a[href='#/tags/new/']");
+    return elementNewTag.click();
+});
+
+When('I create a tag with name {kraken-string}', async function (name) {
+    let elementTitle = await this.driver.$("#tag-name");
+    await elementTitle.setValue(name);
+});
+
+Then('I check the post name {kraken-string}', async function (name) {
+    let postActualName = await this.driver.$(".article-title").getText();
+    expect(postActualName).to.equal(name);
+});
+
+Then('I check the post content {kraken-string}', async function (content) {
+    let postActualContent = await this.driver.$(".article > .gh-content").getText();
+    expect(postActualContent).to.equal(content);
 });
