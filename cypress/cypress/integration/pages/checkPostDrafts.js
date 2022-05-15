4ef855beec746cd76abc43a6722d26521243375a
Steve Andersson Rojas Hernandez <sarojash@unandes.edu.co>
2022-05-15 13:19:09 -0500

    Se agrega escenario 12

--- cypress/cypress/integration/pages/checkPostDrafts.js
+++ cypress/cypress/integration/pages/checkPostDrafts.js
@@ -4,6 +4,7 @@ class checkPostDrafts{
     elements = {
         draftsButton: () => cy.get('.gh-nav-viewname').contains('Drafts'),
         labelTitle: () => cy.get('h3').contains('New-Post'),
+        labelTitleNot: () => cy.get('h3').contains('New-Post'),
     }
 
     // Dar clic al boton de Drafts
@@ -16,6 +17,12 @@ class checkPostDrafts{
     labelTitle = () =>{
         cy.wait(500)
         this.elements.labelTitle().should('have.text', '\n                New-Post\n            ');
+    }
+
+    // Validar titulo del post no este 'New-Post'
+    labelTitleNot = () =>{
+        cy.wait(500)
+        this.elements.labelTitleNot().should('to.not.equal', '\n                New-Post\n            ');
     }
 }
 
